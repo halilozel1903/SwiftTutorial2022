ff476081998accb59319cd76ad048548a1c318cc
halilozel1903 <halilozel1903@gmail.com>
2022-01-23 22:02:06 +0300

    HelloWorld lessons was added.

---
+++ #01-HelloWorld.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// This example is often used introduce a new programming language to beginners.
+print("Hello, World!")
+
+// print(): Displays the "Hello, World!" on your screen.
+
+// We use 2 slashes(//) to write comments in Swift.
+
+
